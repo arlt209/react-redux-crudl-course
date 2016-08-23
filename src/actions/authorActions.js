2c743a3afcd05d73ef51a128c1c3f79b09a2d559
arlt209 <nickarlt@gmail.com>
2016-08-22 17:45:57 -0700

    created ajax STATUS actions and implemented them in the their own reducer and then in the root reducer.

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -2,6 +2,7 @@
 
 import AuthorApi from '../api/mockAuthorApi';
 import * as types from './actionTypes';
+import {beginAjaxCall} from './ajaxStatusActions';
 
 export function loadAuthorSuccess(authors) {
   return {type: types.LOAD_AUTHORS_SUCCESS, authors};
@@ -9,6 +10,7 @@ export function loadAuthorSuccess(authors) {
 
 export function loadAuthors(){
   return dispatch => {
+    dispatch(beginAjaxCall());
     return AuthorApi.getAllAuthors().then(authors => {
       dispatch(loadAuthorSuccess(authors));
     }).catch(error =>{
